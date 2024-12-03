0f1a027377df9ae28007b8a09b1aab69120103ce
lifegame1lu111 <40743123+lifegame1lu111@users.noreply.github.com>
2024-12-03 10:17:15 +0000

    Update Day01.lean

--- Day01.lean
+++ Day01.lean
@@ -1,23 +1,23 @@
 import Batteries.Data.String
 import Mathlib.Data.List.Sort
 
-def parse (content : String) : List ℤ × List ℤ :=
+def parse (content : String) : List Int × List Int :=
     let numbers := content
         |>.replace "   " " "
         |>.split (fun c => c == ' ' || c == '\n')
 
-    let rec helper : List ℤ × List ℤ → List String → List ℤ × List ℤ
+    let rec helper : List Int × List Int → List String → List Int × List Int
         | res, x1 :: x2 :: xs => helper ⟨x1.toInt! :: res.fst, x2.toInt! :: res.snd⟩ xs
         | res, _ => ⟨res.fst.mergeSort, res.snd.mergeSort⟩
 
     helper ⟨[], []⟩ numbers
 
-def part1 (ns : List ℤ) (ms : List ℤ) : ℕ := ns
+def part1 (ns : List Int) (ms : List Int) : Nat := ns
     |>.zipWith (· - ·) ms
-    |>.foldl (· + ·.natAbs) (0 : ℕ)
+    |>.foldl (· + ·.natAbs) (0 : Nat)
 
-def part2 (ns : List ℤ) (ms : List ℤ) : ℕ := ns
-    |>.foldl (fun acc n => acc + (n.toNat * (ms |>.filter (· == n) |>.length))) (0 : ℕ)
+def part2 (ns : List Int) (ms : List Int) : Nat := ns
+    |>.foldl (fun acc n => acc + (n.toNat * (ms |>.filter (· == n) |>.length))) (0 : Nat)
 
 def main : IO Unit := do
     let content ← IO.FS.readFile "input1.txt"
